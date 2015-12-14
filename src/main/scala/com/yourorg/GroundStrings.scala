167fd6f13d7afd3563fef495e0836ee792103321
Tom Hicks <hickst@tohono.com>
2015-12-13 21:31:06 -0700

    Add Scala example to lookup grounding information for strings.

---
+++ src/main/scala/com/yourorg/GroundStrings.scala
@@ -0,0 +1,48 @@
+package com.yourorg
+
+import java.io.File
+
+import edu.arizona.sista.odin._
+import edu.arizona.sista.reach._
+import edu.arizona.sista.reach.grounding._
+
+/**
+ * Lookup grounding information for each string in a list of strings.
+ * Author: Tom Hicks. 12/13/2015.
+ * Last Modified: Initial working version.
+ */
+object GroundStrings extends App {
+
+  /** Specify the lookup tables to be loaded and the order in which they are searched. */
+  val searchSequence =
+    Seq(
+      new StaticProteinFamilyKBLookup,
+      new StaticProteinKBLookup,
+      new StaticChemicalKBLookup,
+      new StaticMetaboliteKBLookup,
+      new StaticCellLocationKBLookup
+    )
+
+  /** Search the sequence of KBs for the given string, returning grounding info if found. */
+  def resolveString (aString: String): Option[Tuple2[String, String]] = {
+    searchSequence.foreach { kbLookup =>     // for each KB in the sequence
+      val refID = kbLookup.resolve(aString)  // lookup the given string key
+      if (!refID.isEmpty)                    // if an entry with that key is found in a KB
+        return Some(Tuple2(kbLookup.namespace, refID.get)) // then return KB namespace and ID value
+    }
+    return None                             // else signal failure to find key string in any KB
+  }
+
+  /** A sample set of strings to be resolved by the grounding machinery. */
+  val model = List("erk1", "RAS", "vanadocene", "Glutaminate", "cell body", "NOT-IN-KB")
+
+  // try to resolve each of the strings in the model:
+  model.foreach { str =>
+    val resolved = resolveString(str)
+    if (resolved.isEmpty)
+      println(s"String '${str}' => NOT FOUND")
+    else
+      println(s"String '${str}' => ${resolved.get._1}(${resolved.get._2})")
+  }
+
+}
