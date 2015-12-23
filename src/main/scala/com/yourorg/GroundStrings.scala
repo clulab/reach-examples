e5124e2e682f239a4d4eadd77e177b32ac508ff7
Tom Hicks <hickst@tohono.com>
2015-12-23 07:41:58 -0700

    Update GroundStrings example to use internal model or take strings from stdin.

--- src/main/scala/com/yourorg/GroundStrings.scala
+++ src/main/scala/com/yourorg/GroundStrings.scala
@@ -1,12 +1,14 @@
 package com.yourorg
 
+import scala.io.Source
+
 import edu.arizona.sista.reach._
 import edu.arizona.sista.reach.grounding._
 
 /**
  * Lookup grounding information for each string in a list of strings.
  * Author: Tom Hicks. 12/13/2015.
- * Last Modified: Cleanups: remove extraneous imports, enhance doc string, trigger HMDB.
+ * Last Modified: Update to optionally read model or use strings from standard input.
  */
 object GroundStrings extends App {
 
@@ -20,6 +22,9 @@ object GroundStrings extends App {
       new StaticCellLocationKBLookup
     )
 
+  /** A sample set of strings to be resolved by the grounding machinery. */
+  val model = List("erk1", "RAS", "vanadocene", "eta-Tocopherol", "cell body", "NOT-IN-KB")
+
   /** Search the sequence of KBs for the given string, returning an optional tuple
       of KB namespace and reference ID, if a KB entry with the given key was found. */
   def resolveString (aString: String): Option[Tuple2[String, String]] = {
@@ -31,11 +36,8 @@ object GroundStrings extends App {
     return None                             // else signal failure to find key string in any KB
   }
 
-  /** A sample set of strings to be resolved by the grounding machinery. */
-  val model = List("erk1", "RAS", "vanadocene", "eta-Tocopherol", "cell body", "NOT-IN-KB")
-
-  // try to resolve each of the strings in the model:
-  model.foreach { str =>
+  /** Try to resolve the given string and print the result of attempt. */
+  def resolveAndPrint (str: String): Unit = {
     val resolved = resolveString(str)
     if (resolved.isEmpty)
       println(s"String '${str}' => NOT FOUND")
@@ -43,4 +45,23 @@ object GroundStrings extends App {
       println(s"String '${str}' => ${resolved.get._1}(${resolved.get._2})")
   }
 
+  /** Try to resolve each of the strings in the sample model. */
+  def doSampleModel (): Unit = {
+    model.foreach(resolveAndPrint(_))
+  }
+
+  // Main method: resolve model strings or resolve each line of standard input
+  var lineCount = 0
+  val start = java.lang.System.currentTimeMillis()
+  if (!args.isEmpty && args(0) == "model") {
+    lineCount = model.size
+    doSampleModel
+  }
+  else
+    for (line <- io.Source.stdin.getLines) {
+      resolveAndPrint(line)
+      lineCount += 1
+    }
+  val stop = java.lang.System.currentTimeMillis()
+  println(s"Resolution time for ${lineCount} lines: ${stop - start}ms")
 }
